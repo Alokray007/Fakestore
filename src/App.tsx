f43613efa2e238f740e31e82b57cc818557703ac
Alokray007 <alokr6927@gmail.com>
2024-06-12 21:24:18 +0530

    Product data Fetching 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 export default function App() {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((json) => console.log(json));
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      
+    </div>
+
+);
 }
