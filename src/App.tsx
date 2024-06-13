160d6b752897389b7647573984c88c282fe63625
Alokray007 <alokr6927@gmail.com>
2024-06-13 23:21:08 +0530

    Product data Fetching 2 - folder structure created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-export default function App() {
-  fetch("https://fakestoreapi.com/products")
-    .then((res) => res.json())
-    .then((json) => console.log(json));
+
 
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      
+
     </div>
 
 );
---
+++ src/services/api.ts
@@ -0,0 +1,4 @@
+export const fetchProducts = async () => {
+    const response = await fetch('https://fakestoreapi.com/products');
+    return response.json();
+};
