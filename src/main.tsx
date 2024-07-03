9070c5fc82ca14dd5e845e7d538b1b3b0b18f1b4
Alokray007 <alokr6927@gmail.com>
2024-07-04 00:29:55 +0530

    Implimet React-Toastify start
    Add ToastContainer inside main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 // import { AppProvider } from './context/context.tsx'
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
     <React.StrictMode>
       <BrowserRouter>
       <App />
+      <ToastContainer />
       </BrowserRouter>
     </React.StrictMode>
 ,
