9ca8967f95862358b5f3c397c6249ab04e913ccd
Alokray007 <alokr6927@gmail.com>
2024-07-04 02:32:37 +0530

    Use localStorage to fetch product details and show in cart UI

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { CartPdct } from '../../types/Products';
 
 const Cart: React.FC = () => {
+  const cart = JSON.parse(localStorage.getItem('cart') || '[]');
+
   return (
     <section className="bg-white py-32 antialiased md:py-32">
       <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
         <h2 className="text-xl font-semibold text-gray-900 sm:text-2xl">Shopping Cart</h2>
-
+        <p className="text-x font-medium text-gray-900 sm:text-xl">Total: <span>{cart?.length}</span>{" "}Items</p>
         <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
+        {cart.length === 0 ? (
+          <div>Cart is Empty</div>
+        ) : (
           <div className="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl">
             <div className="space-y-6">
+              {cart.map((item:CartPdct) => (
               <div className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm md:p-6">
                 <div className="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
                   <a href="#" className="shrink-0 md:order-1">
-                    <img className="h-20 w-20" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg" alt="imac image" />
+                    <img className="h-20 w-20" src={item.image} alt={item.title} />
                   </a>
 
                   <label htmlFor="counter-input" className="sr-only">Choose quantity:</label>
@@ -24,7 +31,7 @@ const Cart: React.FC = () => {
                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16" />
                         </svg>
                       </button>
-                      <input type="text" id="counter-input" data-input-counter className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0" placeholder="" value="2" required />
+                      <input type="text" id="counter-input" data-input-counter className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0" placeholder="" value={item.quantity} required />
                       <button type="button" id="increment-button" data-input-counter-increment="counter-input" className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100">
                         <svg className="h-2.5 w-2.5 text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
@@ -32,12 +39,12 @@ const Cart: React.FC = () => {
                       </button>
                     </div>
                     <div className="text-end md:order-4 md:w-32">
-                      <p className="text-base font-bold text-gray-900">$1,499</p>
+                      <p className="text-base font-bold text-gray-900">${item.price}</p>
                     </div>
                   </div>
 
                   <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
-                    <a href="#" className="text-base font-medium text-gray-900 hover:underline">PC system All in One APPLE iMac (2023) mqrq3ro/a, Apple M3, 24" Retina 4.5K, 8GB, SSD 256GB, 10-core GPU, Keyboard layout INT</a>
+                    <a href="#" className="text-base font-medium text-gray-900 hover:underline">{item.title}</a>
 
                     <div className="flex items-center gap-4">
                       <button type="button" className="inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-900 hover:underline">
@@ -57,9 +64,10 @@ const Cart: React.FC = () => {
                   </div>
                 </div>
               </div>
+              ))}
             </div>
           </div>
-
+          )}
           <div className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
             <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm sm:p-6">
               <p className="text-xl font-semibold text-gray-900">Order summary</p>
