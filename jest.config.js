2957e62f500d856dfb3d7c94166a5ec07d544545
Alokray007 <alokr6927@gmail.com>
2024-07-14 04:30:25 +0530

    Add new handler in moduleNameMapper to manage png files for Tests

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ export default {
   },
 
   moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "identity-obj-proxy",
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
     "^.+\\.svg$": "jest-transformer-svg",
     "^@/(.*)$": "<rootDir>/src/$1",
