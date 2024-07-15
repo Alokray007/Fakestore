d8617d6fd500239dc938c8a364af3d85c7ae22e7
Alokray007 <alokr6927@gmail.com>
2024-07-15 19:10:52 +0530

    Add test for RatingStar component - 2

--- src/components/UI/RatingStar.test.tsx
+++ src/components/UI/RatingStar.test.tsx
@@ -9,10 +9,22 @@ describe('RatingStar component', () => {
         render(<RatingStars {...props} />);
     }
 
-    test('renders the correct number of stars based on the rating', () => {
+    test('renders the correct number of stars based on rating', () => {
         renderComponent(0);
         const stars = document.querySelectorAll('svg');
         expect(stars.length).toBe(5);
+    });
+
+    test('fills the correct number of stars based on rating', () => {
+        renderComponent(3);
+        const stars = document.querySelectorAll('svg');
+        stars.forEach((star, index) => {
+            if(index < 3) {
+                expect(star).toHaveAttribute('fill', 'currentColor')
+            } else {
+                expect(star).toHaveAttribute('fill', 'none')
+            }
+        });
     });
 
 })
