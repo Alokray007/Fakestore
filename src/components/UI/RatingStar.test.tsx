d142136906287498436c6e546e2699795fe9190a
Alokray007 <alokr6927@gmail.com>
2024-07-15 19:13:29 +0530

    Add test for RatingStar component - 3

--- src/components/UI/RatingStar.test.tsx
+++ src/components/UI/RatingStar.test.tsx
@@ -9,7 +9,7 @@ describe('RatingStar component', () => {
         render(<RatingStars {...props} />);
     }
 
-    test('renders the correct number of stars based on rating', () => {
+    test('renders the correct number of stars', () => {
         renderComponent(0);
         const stars = document.querySelectorAll('svg');
         expect(stars.length).toBe(5);
@@ -17,6 +17,18 @@ describe('RatingStar component', () => {
 
     test('fills the correct number of stars based on rating', () => {
         renderComponent(3);
+        const stars = document.querySelectorAll('svg');
+        stars.forEach((star, index) => {
+            if(index < 3) {
+                expect(star).toHaveAttribute('fill', 'currentColor')
+            } else {
+                expect(star).toHaveAttribute('fill', 'none')
+            }
+        });
+    });
+
+    test('rounds the rating to the nearest whole number', () => {
+        renderComponent(2.6);
         const stars = document.querySelectorAll('svg');
         stars.forEach((star, index) => {
             if(index < 3) {
