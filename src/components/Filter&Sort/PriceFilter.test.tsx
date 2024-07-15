f243eb94211a11963f4bd78e3f69fae828b579e1
Alokray007 <alokr6927@gmail.com>
2024-07-16 00:33:43 +0530

    Add test for PriceFilter Component - 1
    It renders all components correctly

---
+++ src/components/Filter&Sort/PriceFilter.test.tsx
@@ -0,0 +1,70 @@
+// check renders all component
+// check if it displays max price
+// check if it calls handleminprice when min price is set
+// ckecl if it calls handlemaxprice when max price is set
+//check when reset is presses it calls handlereset
+
+import { render, screen } from "@testing-library/react";
+import { PriceFilterProps } from "../../types/Products";
+import { Pdct } from "../../types/Products";
+import PriceFilter from "./PriceFilter";
+
+const mockFinalFilter:Pdct[] = [
+    {
+        id: 1,
+        title: 'Product 1',
+        price: 100,
+        category: 'Category 1',
+        description: 'Description 1',
+        image: 'image1.jpg',
+        rating: {
+          rate: 4.5,
+          count: 10,
+        },
+      },
+      {
+        id: 2,
+        title: 'Product 2',
+        price: 200,
+        category: 'Category 2',
+        description: 'Description 2',
+        image: 'image2.jpg',
+        rating: {
+          rate: 4.0,
+          count: 20,
+        },
+    }
+]
+const mockHandleResetPrice = jest.fn();
+const mockHandleMaxPrice = jest.fn();
+const mockHandleMinPrice = jest.fn();
+
+const defaultProps: PriceFilterProps = {
+    FinalFilter: mockFinalFilter,
+    handleResetPrice: mockHandleResetPrice,
+    handleMinPriceChange: mockHandleMinPrice,
+    handleMaxPriceChange: mockHandleMaxPrice,
+    minPrice: 0,
+    maxPrice: 300,
+};
+
+const renderComponent = (props = defaultProps) => {
+    render(<PriceFilter {...props} />);
+}
+
+describe('PriceFilter Component', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('renders all components correctly' ,() => {
+        renderComponent();
+        expect(screen.getByText("Price")).toBeInTheDocument();
+        expect(screen.getByText(/The highest price is/i)).toBeInTheDocument();
+        expect(screen.getByRole("button")).toBeInTheDocument();
+        const inputs = screen.getAllByRole('spinbutton')
+        inputs.forEach(input => {
+            expect(input).toBeInTheDocument();
+        });
+    });
+});
