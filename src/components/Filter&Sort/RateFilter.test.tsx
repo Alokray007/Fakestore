a9b89ae0b289fc54eab4a222a946e8f9ad6f4bda
Alokray007 <alokr6927@gmail.com>
2024-07-16 03:28:03 +0530

    Add test for RateFilter Component - 3
    It displays the correct number of selected ratings

--- src/components/Filter&Sort/RateFilter.test.tsx
+++ src/components/Filter&Sort/RateFilter.test.tsx
@@ -1,40 +1,67 @@
 import { render, screen } from "@testing-library/react";
 import RateFilter from "./RateFilter";
-import {RateFilterProps} from "../../types/Products"
+import { RateFilterProps } from "../../types/Products";
 import userEvent from "@testing-library/user-event";
 
 const mockHandleResetRating = jest.fn();
 const mockHandleRatingChange = jest.fn();
-const selectedRatings = { fourStar: true,  threeStar: true, twoStar: false, oneStar: false }
 
-const renderComponent = () => {
-    const props : RateFilterProps = {handleResetRating : mockHandleResetRating , handleRatingChange : mockHandleRatingChange, selectedRatings: selectedRatings}
-    render(<RateFilter {...props} />)
-}
+const defaultProps: RateFilterProps = {
+  selectedRatings: {
+    fourStar: false,
+    threeStar: false,
+    twoStar: false,
+    oneStar: false,
+  },
+  handleResetRating: mockHandleResetRating,
+  handleRatingChange: mockHandleRatingChange,
+};
 
-describe('RateFilter Component', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-        renderComponent();
+const renderComponent = (props = defaultProps) => {
+  render(<RateFilter {...props} />);
+};
+
+describe("RateFilter Component", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("renders all components correctly", () => {
+    renderComponent();
+
+    expect(screen.getByText("Rating")).toBeInTheDocument();
+    expect(screen.getByText(/Selected/i)).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /Reset/i })).toBeInTheDocument();
+    const allRatings = screen.getAllByRole("checkbox");
+    allRatings.forEach((rating) => {
+      expect(rating).toBeInTheDocument();
     });
+  });
 
-    test('renders all components correctly', () => {
-        expect(screen.getByText("Rating")).toBeInTheDocument();
-        expect(screen.getByText(/Selected/i)).toBeInTheDocument();
-        expect(screen.getByRole("button", {name: /Reset/i})).toBeInTheDocument();
-        const allRatings = screen.getAllByRole("checkbox");
-        allRatings.forEach(rating => {
-            expect(rating).toBeInTheDocument();
-        })
-    });
+  test("calls handleResetRating when reset button is clicked", async () => {
+    renderComponent();
 
-    test('calls handleResetRating when reset button is clicked', async() => {
-        const resetButton = screen.getByRole("button", {name: /Reset/i});
+    const resetButton = screen.getByRole("button", { name: /Reset/i });
 
-        const user = userEvent.setup();
-        await user.click(resetButton);
+    const user = userEvent.setup();
+    await user.click(resetButton);
 
-        expect(mockHandleResetRating).toHaveBeenCalled();
-    })
+    expect(mockHandleResetRating).toHaveBeenCalled();
+  });
 
+  test("displays the correct number of selected ratings", async () => {
+    const props = {
+        ...defaultProps,
+        selectedRatings: {
+          fourStar: true,
+          threeStar: true,
+          twoStar: false,
+          oneStar: false,
+        },
+      };
+    renderComponent(props);
+
+    expect(screen.getByText(/Selected/i)).toHaveTextContent("2 Selected");
+  });
+  
 });
