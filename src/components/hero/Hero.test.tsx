62800e74a55018fafd2915b4575aaf290dd5be0f
Alokray007 <alokr6927@gmail.com>
2024-07-16 19:05:52 +0530

    Add test for Hero Component - 4
    it displays product information when fetched successfully

--- src/components/hero/Hero.test.tsx
+++ src/components/hero/Hero.test.tsx
@@ -1,10 +1,9 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import Hero from "./Hero";
-// import { Pdct } from "@/types/Products";
+import { Pdct } from "@/types/Products";
 import axios from "../../services/axios";
 import MockAdapter from "axios-mock-adapter";
 import { BrowserRouter as Router } from "react-router-dom";
-
 
 // Mock toast
 jest.mock("react-toastify", () => ({
@@ -15,42 +14,55 @@ jest.mock("react-toastify", () => ({
 
 const mockAxios = new MockAdapter(axios);
 
-// const mockProducts: Pdct = {
-//   id: 1,
-//   title: "Product 1",
-//   price: 100,
-//   category: "Category 1",
-//   description: "Description 1",
-//   image: "image1.jpg",
-//   rating: {
-//     rate: 4.5,
-//     count: 10,
-//   },
-// };
+const mockProducts: Pdct = {
+  id: 1,
+  title: "Product 1",
+  price: 100,
+  category: "Category 1",
+  description: "Description 1",
+  image: "image1.jpg",
+  rating: {
+    rate: 4.5,
+    count: 10,
+  },
+};
 
 describe("Hero Component", () => {
   beforeEach(() => {
     mockAxios.reset();
     jest.clearAllMocks();
     render(
-        <Router>
-          <Hero />
-        </Router>
-      );
+      <Router>
+        <Hero />
+      </Router>
+    );
   });
 
-  test('renders the spinner while fetching data', () => {
-    mockAxios.onGet('/products?limit=15').reply(200,[]);
-    expect(screen.getByText('Loading...')).toBeInTheDocument();
+  test("renders the spinner while fetching data", () => {
+    mockAxios.onGet("/products?limit=15").reply(200, []);
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
   });
 
-  test('displays error message on fetch failure', async() => {
-    mockAxios.onGet('/products?limit=15').reply(500);
+  test("displays error message on fetch failure", async () => {
+    mockAxios.onGet("/products?limit=15").reply(500);
     await waitFor(() => {
-        expect(screen.getByText(/Request failed with status code/i)).toBeInTheDocument();
+      expect(
+        screen.getByText(/Request failed with status code/i)
+      ).toBeInTheDocument();
     });
   });
 
-  
+  test("displays product information when fetched successfully", async () => {
+    mockAxios.onGet("/products?limit=15").reply(200, [mockProducts]);
+    render(
+      <Router>
+        <Hero />
+      </Router>
+    );
 
+    await waitFor(() => {
+      expect(screen.getByText("Before they sold out")).toBeInTheDocument();
+      expect(screen.getByText(mockProducts.title)).toBeInTheDocument();
+    });
+  });
 });
