5e30fe28c5e7aa7e63d04fec3565b701e88b4cf1
Alokray007 <alokr6927@gmail.com>
2024-07-16 15:15:40 +0530

    Add test for Sort Component - 4
    It should allow user to change Sort filter

--- src/components/Filter&Sort/Sort.test.tsx
+++ src/components/Filter&Sort/Sort.test.tsx
@@ -18,17 +18,28 @@ describe('Sort Component', () => {
 
     test('renders all components correctly', () => {
         expect(screen.getByLabelText('Sort By')).toBeInTheDocument();
-        expect(screen.getByRole('combobox')).toBeInTheDocument();
+        expect(screen.getByRole('combobox', { name: /sort by/i })).toBeInTheDocument();
     });
 
     test('should correctly set default option', () => {
-        const defaultOption = screen.getAllByRole('option');
-        expect(defaultOption[0]).toHaveTextContent('Sort By');
+        expect((screen.getByRole('option', { name: 'Sort By' })as HTMLOptionElement).selected).toBe(true)
     })
 
     test('should display the correct number of options', () => {
         const options = screen.getAllByRole('option');
         expect(options).toHaveLength(8);
     });
+
+    test ('should allow user to change Sort filter', async() => {
+        const user = userEvent.setup();
+        const dropdown = screen.getByRole('combobox', { name: /sort by/i });
+
+        expect((screen.getByRole('option', { name: 'Sort By' }) as HTMLOptionElement).selected).toBe(true);
+
+        await user.selectOptions(dropdown, 'Price: High to Low');
+
+        expect((screen.getByRole('option', { name: 'Price: High to Low' }) as HTMLOptionElement).selected).toBe(true);
+    });
+
 
 });
