4a42e7b34644eda62d3d5b6ec7519efd8c2fee1e
Alokray007 <alokr6927@gmail.com>
2024-07-16 18:31:47 +0530

    Add test for Hero Component - 2
    It displays error message on fetch failure

--- src/components/hero/Hero.test.tsx
+++ src/components/hero/Hero.test.tsx
@@ -1,6 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Hero from "./Hero";
-import { Pdct } from "@/types/Products";
+// import { Pdct } from "@/types/Products";
 import axios from "../../services/axios";
 import MockAdapter from "axios-mock-adapter";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -15,18 +15,18 @@ jest.mock("react-toastify", () => ({
 
 const mockAxios = new MockAdapter(axios);
 
-const mockProducts: Pdct = {
-  id: 1,
-  title: "Product 1",
-  price: 100,
-  category: "Category 1",
-  description: "Description 1",
-  image: "image1.jpg",
-  rating: {
-    rate: 4.5,
-    count: 10,
-  },
-};
+// const mockProducts: Pdct = {
+//   id: 1,
+//   title: "Product 1",
+//   price: 100,
+//   category: "Category 1",
+//   description: "Description 1",
+//   image: "image1.jpg",
+//   rating: {
+//     rate: 4.5,
+//     count: 10,
+//   },
+// };
 
 describe("Hero Component", () => {
   beforeEach(() => {
@@ -42,6 +42,13 @@ describe("Hero Component", () => {
   test('renders the spinner while fetching data', () => {
     mockAxios.onGet('/products?limit=15').reply(200,[]);
     expect(screen.getByText('Loading...')).toBeInTheDocument();
+  })
+
+  test('displays error message on fetch failure', async() => {
+    mockAxios.onGet('/products?limit=15').reply(500);
+    await waitFor(() => {
+        expect(screen.getByText(/Request failed with status code/i)).toBeInTheDocument();
+    });
   })
 
 });
