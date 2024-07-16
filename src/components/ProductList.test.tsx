d8ed248f2fb8ded82ed001bb2a49871e6b705614
Alokray007 <alokr6927@gmail.com>
2024-07-17 00:30:20 +0530

    Add test for ProductList Component - 2
    It display all products correctly

--- src/components/ProductList.test.tsx
+++ src/components/ProductList.test.tsx
@@ -2,6 +2,7 @@ import { ProductListProps } from "../types/Products";
 import { BrowserRouter as Router } from "react-router-dom";
 import ProductList from "./ProductList";
 import { render, screen } from "@testing-library/react";
+// import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const mockProducts: ProductListProps['products'] = [
     {
@@ -43,6 +44,23 @@ describe('ProductList Component', () => {
         );
 
         expect(screen.getByText('Loading...')).toBeInTheDocument();
+    });
+
+    test('display all products correctly', () => {
+        render(
+            <Router>
+                <ProductList products={mockProducts} />
+            </Router>
+        );
+        mockProducts.forEach(product => {
+            expect(screen.getByText(product.title)).toBeInTheDocument();
+            expect(screen.getByText(`Price: $${product.price}`)).toBeInTheDocument();
+            expect(screen.getByText(product.rating.rate.toString())).toBeInTheDocument();
+            expect(screen.getByText(`(${product.rating.count})`)).toBeInTheDocument();
+            const image = screen.getByAltText(product.title);
+            expect(image).toBeInTheDocument();
+            expect(image).toHaveAttribute('src', product.image);
+        });
     });
 
 
