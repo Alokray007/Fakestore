bd1e28128a070856240e580897ca58fdf4e91322
Alokray007 <alokr6927@gmail.com>
2024-07-16 23:45:21 +0530

    Add test for CategoryFilter Component - 1
    It fetches and displays categories correctly

---
+++ src/components/CategoryFilter.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import axios from '../services/axios';
+import MockAdapter from 'axios-mock-adapter';
+import CategoryFilter from './CategoryFilter';
+
+// Mock axios
+const mockAxios = new MockAdapter(axios);
+
+const mockCategories = ['Electronics', 'Jewelery', 'Men\'s clothing', 'Women\'s clothing'];
+
+describe('CategoryFilter Component', () => {
+    beforeEach(() => {
+        mockAxios.reset();
+        jest.clearAllMocks();
+    });
+
+    test('fetches and displays categories correctly', async() => {
+        mockAxios.onGet('/products/categories').reply(200, mockCategories);
+        render(<CategoryFilter onCategoryChange={jest.fn()}/>);
+
+        await waitFor(() => {
+            mockCategories.forEach(category => {
+              expect(screen.getByText(category)).toBeInTheDocument();
+            });
+        });
+    });
+
+    ;
+
+});
